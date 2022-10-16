434e950e3faabab6fc4297081b12d00d9a1a6c92
Ali Raza <aliraza.2369196@gmail.com>
2022-10-16 13:48:15 +0500

    first comit again

---
+++ README.md
@@ -0,0 +1,5 @@
+## Kiridating.com Web page
+
+-- Download the zip-archive branch main.
+-- Go to the panel file manager.
+-- In the file manager, put the files from the downloaded branch/folder kiridating.com into the kiridating.com folder.
---
+++ component/sidebar.php
@@ -0,0 +1,61 @@
+<div class="sidebar">
+  <div class="sidebar-logo"><a href="https://www.kiridating.com"
+                               class="sidebar-logo-item">        <picture>
+                                                                           <source srcset="./images/kiri..webp" type="image/webp">
+                                                                            <source srcset="./images/kiri..jpg" type="image/jpeg">
+                                                                                     <img class="sidebar-logo-item-img" src="./images/kiri..jpg" alt="Logo">
+                                                                       </picture>
+                                                                           <picture>
+                                                                                 <source srcset="./images/kiri-mob.webp" type="image/webp">
+                                                                                 <source srcset="./images/kiri-mob.jpg" type="image/jpeg">
+                                                                               <img class="sidebar-logo-item-img-mobile" src="./images/kiri-mob.jpg" alt="Logo">
+                                                                                                                                              </picture>
+                                                                       </a>
+                                                                       </div>
+
+  <ul class="sidebar-menu">
+    <li class="sidebar-menu-link about" ><a
+      href="https://www.kiridating.com/#about" data-link="#about">About</a>
+    </li>
+    <li class="sidebar-menu-link help" ><a
+      href="https://www.kiridating.com/#help" data-link="#help">Help</a>
+    </li>
+    <li class="sidebar-menu-link alpha-plus" ><a
+      href="https://www.kiridating.com/#kiri" data-link="#kiri">Kiri Premium</a>
+    </li>
+    <li class="sidebar-menu-link blog" >
+      <a
+        href="https://www.kiridating.com/#blog" data-link="#blog">Blog</a>
+    </li>
+    <li class="sidebar-menu-link equality" >
+      <a
+        href="https://www.kiridating.com/#equality" data-link="#alphaForEquality">Kiri for Equality</a>
+    </li>
+    <li class="sidebar-menu-link careers" >
+      <a href="https://www.kiridating.com/#careers" data-link="#careers">Careers</a>
+    </li>
+    <li class="sidebar-menu-link terms" >
+      <a
+        href="https://www.kiridating.com/#terms" data-link="#termsOfService">Terms of Service</a>
+    </li>
+    <li class="sidebar-menu-link privacy" ><a
+      href="https://www.kiridating.com/privacy.php" data-link="#privacy">Privacy Policy</a>
+    </li>
+    <li class="sidebar-menu-link cookie" ><a
+      href="https://www.kiridating.com/#cookie" data-link="#cookie">Cookie Policy</a>
+    </li>
+    <li class="sidebar-menu-link eula" ><a
+      href="https://www.kiridating.com/#eula" data-link="#eula">EULA</a>
+    </li>
+       <li class="sidebar-menu-link" ><a
+          href="#" ></a>
+        </li>
+  </ul>
+
+  <div id="burger">
+      <div class="line1"></div>
+      <div class="line2"></div>
+      <div class="line3"></div>
+    </div>
+
+</div>
